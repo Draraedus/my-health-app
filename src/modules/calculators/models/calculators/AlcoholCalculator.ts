7e1634c6e3f4a872a46d86376c3989b2342eb653
victor <victorbakugan1@hotmail.com>
2023-05-01 11:36:53 -0300

    Adicionar tela de calculadora álcool no sangue

---
+++ src/modules/calculators/models/calculators/AlcoholCalculator.ts
@@ -0,0 +1,49 @@
+import HealthCalculator from './HealthCalculators'
+
+export default class AlcoholCalculator implements HealthCalculator {
+  protected volume: number
+  protected percentual: number
+  protected weight: number
+
+  public date: Date
+  public result: number
+  public description: string
+
+  constructor() {
+    this.volume = 0
+    this.weight = 0
+    this.percentual = 0
+    this.date = new Date()
+    this.result = 0
+    this.description =
+      'O Teor Álcoolico Sanguíneo (TAS) é uma medida que avalia a concentração de álcool circulado no sangue, bem como a determinação e avaliação dos seus efeitos. O cálculo obtido é pelo volume de álcool ingerido (em ml) vezes o percentual de teor álcoolico da bebida ingerida (em %) vezes a taxa fixa de 0.8, e dividido pelo seu peso (em kg) vezes 0.6. Esse resultado trará a concentração de álcool estimada circulando em seu sangue, trazendo informações medicinais sobre o atual estado em que um indivíduo analisado se encontra, seus possíveis sintomas, os níveis do seu consumo, e o seu tipo de intoxicação (sendo leve, moderado, grave e até fatal). Por ser uma calculadora simplificada, ela não dita necessariamente alguns dos fatores importantes que podem ser levados em conta no resultado final, como o número de horas passadas após o consumo, o sexo, entre outros.'
+  }
+
+  public setVolume(volume: number): void {
+    this.volume = volume
+  }
+  public setPercentual(percentual: number): void {
+    this.percentual = percentual
+  }
+  public setWeight(weight: number): void {
+    this.weight = weight
+  }
+
+  public calculate() {
+    this.result = (this.volume * this.percentual * 0.8) / (this.weight * 0.6)
+  }
+
+  printResultDescription(): string {
+    if (this.result <= 100) {
+      return 'O valor resultante está abaixo dos 100 mg/dl, o que indica uma Intoxicação/Consumo leve. Alguns dos sintomas possíveis são o déficit de atenção e memória, vermelhidão na pele e batimentos cardíacos acelerados.'
+    } else if (this.result > 100 && this.result <= 250) {
+      return 'O valor resultante está entre 100 e 250mg/dl, o que indica uma Intoxicação/Consumo moderado. Alguns dos sintomas possíveis são a instabilidade física e do humor, tontura, náuseas e vômitos, amnésia.'
+    } else if (this.result > 250 && this.result <= 400) {
+      return 'O valor resultante está entre 250 e 400mg/dl, o que indica uma Intoxicação/Consumo grave. Alguns dos sintomas possíveis são a hipotermia, tontura severa, dificuldade na fala, além de delírios e alucinações.'
+    } else if (this.result > 400) {
+      return 'O valor resultante está acima de 400ng/dl, o que indica uma Intoxicação/Consumo potencialmente fatal. Nesse estado, o paciente pode apresentar um coma potencial, convulsão, perda de consciência e habilidades motoras, insuficiência respiratória, e até mesmo lesões fatais.'
+    } else {
+      return ''
+    }
+  }
+}
--- src/modules/calculators/pages/AlcoholInBlood.tsx
+++ src/modules/calculators/pages/AlcoholInBlood.tsx
@@ -1,11 +1,16 @@
-import { View } from 'react-native'
+import { useRef, useState } from 'react'
+import { View, StyleSheet, ScrollView } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-
-import { globalStyles } from '@shared/ui/globalStyles'
-import { StyledText } from '@shared/ui/components'
-
-import { AllCalculators } from '../calculatorsEnum'
 import { RouteParams } from '../../../routeParams'
+import { globalStyles } from '@shared/ui/globalStyles'
+import { GRAY_300, GREEN_600 } from '@shared/ui/colors'
+import { TextBox, TextButton } from '@shared/ui/components'
+import TittleContainer from '../components/CalculatorTittle'
+import DescriptionContainer from '../components/CalculatorDescription'
+import { CalculatorFormField } from '../components/CalculatorFormField'
+import ResultCalculateContainer from '../components/CalculatorResult'
+import { AllCalculators } from '../calculatorsEnum'
+import AlcoholCalculator from '../models/calculators/AlcoholCalculator'
 
 type CalculatorsProps = NativeStackScreenProps<
   RouteParams,
@@ -13,9 +18,115 @@ type CalculatorsProps = NativeStackScreenProps<
 >
 
 export default function AlcoholInBlood(props: CalculatorsProps) {
+  const alcohol = new AlcoholCalculator()
+
+  const [weight, setWeight] = useState(0)
+  const [volume, setVolume] = useState(0)
+  const [percentual, setPercentual] = useState(0)
+  const [result, setResult] = useState(0)
+  const [resultDescription, setResultDescription] = useState('')
+
+  const scrollViewRef = useRef<ScrollView>(null)
+
+  const handleScroll = (event: {
+    nativeEvent: { contentOffset: { y: number } }
+  }) => {
+    const offsetY = event.nativeEvent.contentOffset.y
+    if (offsetY === 0) {
+      scrollViewRef.current?.scrollTo({ y: -100, animated: true })
+    }
+  }
+
+  function calculateAlcoholInBlood() {
+    alcohol.setWeight(weight)
+    alcohol.setPercentual(percentual)
+    alcohol.setVolume(volume)
+    alcohol.calculate()
+    setResultDescription(alcohol.printResultDescription())
+    setResult(alcohol.result)
+  }
+
   return (
-    <View style={globalStyles.defaultContainer}>
-      <StyledText>Hello, world</StyledText>
-    </View>
+    <ScrollView
+      onScroll={handleScroll}
+      ref={scrollViewRef}
+      contentContainerStyle={styles.primaryContainer}
+    >
+      <TittleContainer name={AllCalculators.AlcoholInBlood} />
+      <DescriptionContainer description={alcohol.description} />
+      <View style={[styles.alcoholCalculateContainer, globalStyles.marginTop4]}>
+        <CalculatorFormField label="Peso: (kg)">
+          <TextBox
+            style={styles.textBoxStyle}
+            keyboardType="numeric"
+            onChangeText={(text) => setWeight(parseFloat(text))}
+            placeholder="Ex.: 50"
+          />
+        </CalculatorFormField>
+        <CalculatorFormField label="Volume Ingerido: (ml)">
+          <TextBox
+            style={styles.textBoxStyle}
+            keyboardType="numeric"
+            onChangeText={(text) => setVolume(parseFloat(text))}
+            placeholder="Ex.: 1000"
+          />
+        </CalculatorFormField>
+        <CalculatorFormField label="Percentual do Teor Álcoolico: (%)">
+          <TextBox
+            style={styles.textBoxStyle}
+            keyboardType="numeric"
+            onChangeText={(text) => setPercentual(parseFloat(text))}
+            placeholder="Ex.: 5"
+          />
+        </CalculatorFormField>
+        <TextButton
+          style={[styles.buttonCalculate, globalStyles.marginTop3]}
+          onPress={calculateAlcoholInBlood}
+        >
+          Calcular
+        </TextButton>
+      </View>
+      {result !== 0 && (
+        <ResultCalculateContainer
+          result={result.toFixed(1).toString()}
+          resultDescription={resultDescription}
+        />
+      )}
+    </ScrollView>
   )
 }
+const styles = StyleSheet.create({
+  primaryContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  alcoholCalculateContainer: {
+    width: '80%',
+    padding: 32,
+
+    borderRadius: 10,
+
+    borderWidth: 1,
+    borderColor: GRAY_300,
+
+    backgroundColor: GREEN_600,
+  },
+  textBoxStyle: {
+    width: '40%',
+    backgroundColor: '#ffffff',
+  },
+  buttonCalculate: {
+    borderWidth: 2,
+    borderColor: GRAY_300,
+    borderRadius: 4,
+    width: '40%',
+    padding: 8,
+    outlineStyle: 'none',
+    backgroundColor: '#ffffff',
+  },
+  textWhite: {
+    color: '#ffffff',
+  },
+})
